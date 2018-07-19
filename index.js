8be7f4fe8fa87d1b27f74ff847f9d36b1c55d28b
DeColloin97 <41453958+DeColloin97@users.noreply.github.com>
2018-07-19 18:42:02 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,3 +6,5 @@ bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
   bot.user.setActivity("discord | -help", {type: "WATCHING"});
 });
+
+bot.login("NDY5MzA0ODc3NzcyNzAxNjk3.DjKYZA.Y9qtEx9wspqsuwCaoj1ueGF2mOk");
